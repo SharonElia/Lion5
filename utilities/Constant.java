e0faee32514f74567604aa4c0f34927007430d61
SharonElia <30668337+SharonElia@users.noreply.github.com>
2017-08-22 20:25:05 +0300

    Add files via upload

---
+++ utilities/Constant.java
@@ -0,0 +1,18 @@
+package SharonProject.utilities;
+
+/**
+ * Created by SMaymonX054905 on 22/08/2017.
+ */
+public class Constant {
+    public static final String URL = "http://toolsqa.com/selenium-training/";
+    public static final String Path_TestData = "/test/SharonT/";
+    public static final String File_TestData = "Report.xls";
+    //public static final String Name = "Sharon";
+ //   public  static final String Email = "Test@123";
+  //  public  static final String Mobile = "0522";
+   // public  static final String Country = "USA";
+   // public  final String YourMessage = "My Name is Sharon";
+
+
+
+}
